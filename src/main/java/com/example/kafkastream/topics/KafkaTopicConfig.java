cc786b78f78de47c39a566c7a6de76402123384a
Gulshan Kumar <kumar.19gulshan95@gmail.com>
2021-09-18 18:08:36 +0530

    the topic names shall be read from the properties file

--- src/main/java/com/example/kafkastream/topics/KafkaTopicConfig.java
+++ src/main/java/com/example/kafkastream/topics/KafkaTopicConfig.java
@@ -2,19 +2,25 @@ package com.example.kafkastream.topics;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaTopicConfig {
 
+    @Value(value = "${kafka.producer.topic.name}")
+    private String produceTo;
+
+    @Value(value = "${kafka.consumer.topic.name}")
+    private String consumeFrom;
     @Bean
     NewTopic quotes(){
-        return new NewTopic("quotes", 3, (short)1);
+        return new NewTopic(produceTo, 3, (short)1);
     }
 
     @Bean
     NewTopic counts(){
-        return new NewTopic("counts", 3, (short)1);
+        return new NewTopic(consumeFrom, 3, (short)1);
     }
 }
